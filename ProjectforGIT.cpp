bd7b064ce8f0f6d708062d7b24d515c2684c2076
sOPIQ-GEj <168332992+sOPIQ-GEj@users.noreply.github.com>
2024-04-28 14:21:29 +0500

    Add files via upload

---
+++ ProjectforGIT.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int triangle_a = 8;
+	vector<char> triangle(triangle_a * triangle_a, '*');
+	for (int i = 0; i < triangle.size(); i++) {
+		if (i % triangle_a == triangle_a - 1) {
+			cout << (i - (i / triangle_a * triangle_a) < i / triangle_a + 1 ? triangle[i] : ' ') << endl;
+		}
+		else {
+			cout << (i - (i / triangle_a * triangle_a) < i / triangle_a + 1 ? triangle[i] : ' ') << ' ';
+		}
+	}
+	return 0;
+}
